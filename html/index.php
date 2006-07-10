5942d0f68be77dd440cc50a5642fe7ab46c4e27e
Mike Ellis <mike@gbnsr.org.uk>
2006-07-10 19:13:30 +0000

    Created a very basic index file for proving that the server-side automatic update scripts work to allow testing and development.

---
+++ html/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<title>Aircraft Technical Log System</title>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
+<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+<style type="text/css" media="all">
+</style>
+</head>
+<body>
+<div id="topstrap">
+</div>
+
+<div id="mainview">
+    <H1>Page Header</H1>
+</div>
+
+<div id="infobar">
+    <H1>Info bar goes here</H1>
+</div>
+
+</body>
+</html>
