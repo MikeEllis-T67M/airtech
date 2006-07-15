22dbd205f8f1b07db19f8c485a4f8ad08fbc7d71
Mike Ellis <mike@gbnsr.org.uk>
2006-07-15 21:42:59 +0000

    First attempt to get some rounded corners working...

--- html/index.php
+++ html/index.php
@@ -2,24 +2,46 @@
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-<title>Aircraft Technical Log System</title>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
-<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
-<style type="text/css" media="all">
-</style>
+  <title>Aircraft Technical Log System</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
+  <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+
+  <style type="text/css">
+    .bordered {
+      border:3px red solid;
+      width:200px;
+      padding:30px;
+    }
+  </style>
+
+  <script type="text/javascript" src="cssquery2-p.js"></script>
+  <script type="text/javascript" src="ruzeeborders.js"></script>
+
+  <script type="text/javascript"><!--
+    // add the border definitions
+    RUZEE.Borders.add({
+      ".bordered" : { borderType:"shadow", cornerRadius:15, shadowWidth:10 }
+    });
+
+    // perform the rendering when the document has been loaded completely
+    window.onload=function(){
+      RUZEE.Borders.render();
+    };
+    //-->
+  </script>
 </head>
 <body>
-<div id="topstrap">
-</div>
+  <div id="topstrap">
+  </div>
 
-<div id="mainview">
+  <div id="bordered">
     <H1>Page Header testing</H1>
     <P>Have I got the permissions right?
-</div>
+  </div>
 
-<div id="infobar">
+  <div id="infobar">
     <H1>Info bar goes here</H1>
-</div>
+  </div>
 
 </body>
 </html>
