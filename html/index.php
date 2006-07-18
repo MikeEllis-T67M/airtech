8dc9789954f5981ba52a682db80a64a492bcb117
Mike Ellis <mike@gbnsr.org.uk>
2006-07-18 18:48:09 +0000

    Tidied up the web-page layout and put in some (hard coded at the moment) hooks for how the fuctionality may work in the future once the PHP is written.

--- html/index.php
+++ html/index.php
@@ -16,7 +16,7 @@
   <script type="text/javascript"><!--
     // add the border definitions
     RUZEE.Borders.add({
-      ".bordered" : { borderType:"shadow", cornerRadius:15, shadowWidth:10 }
+      ".bordered" : { borderType:"shadow", cornerRadius:10, shadowWidth:5 }
     });
 
     // perform the rendering when the document has been loaded completely
@@ -39,15 +39,26 @@
     <div id="layout_left">
       <div class="bordered">
         <div id="navbar" class="content">
-          <H1>Navigation bar</H1>
+          <H1>Technical Log</H1>
+          <FORM NAME="date">
+            <SELECT NAME="SelectDate" onChange="document.location.href=document.nav.SelectURL.options[document.date.SelectDate.selectedIndex].value">
+              <OPTION VALUE="http://www.cs.tut.fi/~jkorpela/forms/jsnav.html" All>All techlog entries</OPTION>
+              <OPTION VALUE="">May 2006</OPTION>
+              <OPTION VALUE="">June 2006</OPTION>
+              <OPTION VALUE="">July 2006</OPTION>
+            </SELECT>
+          </FORM>
+          <H1>Documentation</H1>
+          <P><A HREF="http://www.gbnsr.org.uk/docs/checklist.pdf">Aircraft checklist</A>
         </div>
       </div>
 
       <div class="bordered">
         <div id="links" class="content">
-          <H1>Links</H1>
+          <H1>External links</H1>
           <P><A HREF="http://www.cubair.co.uk">Cubair Flight Training</A>
           <P><A HREF="http://www.redhillaerodrome.com">Redhill Aerodrome</A>
+          <P><A HREF="http://www.met-office.gov.uk/aviation">Weather</A>
           <P><A HREF="http://www.ais.org.uk">NOTAMs</A>
         </div>
       </div>
