ad8e1261d1826b0781897c53fee7f224d4094dd7
Mike Ellis <mike@gbnsr.org.uk>
2006-07-18 21:31:17 +0000

    Trying to undo the damage

--- html/index.php
+++ html/index.php
@@ -3,7 +3,6 @@
 // connected and the user is authenticated
 require 'include/db_params.inc';
 ?>
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
