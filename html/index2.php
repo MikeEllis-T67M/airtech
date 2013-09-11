db34d5f1303a5273b1d0279f43a488b18d7df599
Mike Ellis <mike@gbnsr.org.uk>
2013-09-11 22:35:35 +0000

    Added a second index file with more error logging turned on

---
+++ html/index2.php
@@ -0,0 +1,21 @@
+<?php
+// Turn on the error reporting
+error_reporting(E_ALL);             
+
+// Library includes
+require 'HTML/Template/ITX.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
+// Project includes
+require 'include/db_params.inc';
+require 'include/techlog.inc';
+
+// Initialise the templates library
+$page = new HTML_Template_ITX("templates/");
+$page->loadTemplatefile("main.tpl");
+
+print_techlog($page, 'G-BNSR', NULL, NULL);
+
+$page->show();
+?>
